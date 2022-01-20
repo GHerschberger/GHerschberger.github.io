d1c035abaccb140a38b78a31795535517569a953
GBirdo <91208486+GHerschberger@users.noreply.github.com>
2022-01-20 20:22:01 +0000

    tried some more stuff with my practice html file.

--- test.js
+++ test.js
@@ -6,12 +6,13 @@ var years;
 var compoundInterest;
 var balance;
 
+
 console.log("Welcome to the interest calculator!");
-initialBalance = prompt("Enter the beginning balance: ");
+//initialBalance = prompt("Enter the beginning balance: ");
 initialBalance = parseFloat(initialBalance);
-interestRate = prompt("Enter the annual interest rate as a decimal (ex. 5% is .05): ");
+//interestRate = prompt("Enter the annual interest rate as a decimal (ex. 5% is .05): ");
 interestRate = parseFloat(interestRate);
-years = prompt("Enter the amount of years you will be earning interest: ");
+//years = prompt("Enter the amount of years you will be earning interest: ");
 years = parseFloat(years);
 
 //Calculate the annual interest
@@ -26,8 +27,8 @@ compoundInterest = Math.pow(compoundInterest, years);
 compoundInterest = compoundInterest * initialBalance
 
 //display the interest amount
-document.write("Interest for one year: $" + interest + "<br>");
+document.write("<br><p>Interest for one year: $" + interest + "<br>");
 
 document.write("Your new balance  after 1 year is: $" + balance.toFixed(2) + "<br>");
 
-document.write("total interest after " + years + " years is " + compoundInterest.toFixed(2) );
+document.write("total interest after " + years + " years is " + compoundInterest.toFixed(2) +  "</p>");
