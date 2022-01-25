5ca8bda3c5933231833022eb3cab39eab8ea0800
GBirdo <91208486+GHerschberger@users.noreply.github.com>
2022-01-25 22:43:55 +0000

    tried to make it work. It didn't

--- test.js
+++ test.js
@@ -7,28 +7,26 @@ var compoundInterest;
 var balance;
 
 
-console.log("Welcome to the interest calculator!");
-//initialBalance = prompt("Enter the beginning balance: ");
-initialBalance = parseFloat(initialBalance);
-//interestRate = prompt("Enter the annual interest rate as a decimal (ex. 5% is .05): ");
-interestRate = parseFloat(interestRate);
-//years = prompt("Enter the amount of years you will be earning interest: ");
-years = parseFloat(years);
+function getVal() {
+  const val = document.querySelector('input').value;
+  console.log(val);
+}
 
-//Calculate the annual interest
-interest = initialBalance * interestRate;
-//add the interest
-balance = initialBalance + interest;
+function interestBalance() {
+  console.log("Welcome to the interest calculator!");
+  initialBalance = prompt("Enter the beginning balance: ");
+  initialBalance = parseFloat(initialBalance);
+  interestRate = prompt("Enter the annual interest rate as a decimal (ex. 5% is .05): ");
+  interestRate = parseFloat(interestRate);
+  years = prompt("Enter the amount of years you will be earning interest: ");
+  years = parseFloat(years);
+  interest = initialBalance * interestRate;
+  balance = initialBalance + interest;
+  compoundInterest = 1 + interestRate;
+  compoundInterest = Math.pow(compoundInterest, years);
+  compoundInterest = compoundInterest * initialBalance
+  document.write("<br><p>Interest for one year: $" + interest + "<br>");
 
-//calculate total Interest
-compoundInterest = 1 + interestRate;
-
-compoundInterest = Math.pow(compoundInterest, years);
-compoundInterest = compoundInterest * initialBalance
-
-//display the interest amount
-document.write("<br><p>Interest for one year: $" + interest + "<br>");
-
-document.write("Your new balance  after 1 year is: $" + balance.toFixed(2) + "<br>");
-
-document.write("total interest after " + years + " years is " + compoundInterest.toFixed(2) +  "</p>");
+  document.write("Your new balance  after 1 year is: $" + balance.toFixed(2) + "<br>");
+  document.write("total interest after " + years + " years is " + compoundInterest.toFixed(2) + "</p>");
+}
