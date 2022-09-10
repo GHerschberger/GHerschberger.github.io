2f7c4f4d5e852d9d664ea127f651c3d79dbf85e9
GBirdo <91208486+GHerschberger@users.noreply.github.com>
2022-09-10 20:05:58 +0000

    git commit -m "render pyramid to the DOM instead of the console"

--- mario.js
+++ mario.js
@@ -1,23 +1,51 @@
 height = parseInt(prompt("please enter a pyramid height"));
 
-printPyramid(height);
+// drawPyramid(height);
+// printPyramid(height);
 
-function printPyramid(height) {
-    let space = " ";
+
+function drawPyramid(height) {
+    let space = ".";
     let brick = "#";
     let rows = height;
 
     let layer = space;
-    for (let i = 0; i<rows ; i++){
+    for (let i = 1; i<rows; i++){
       layer += space;
     }
     layer += brick;
       for(let j=0; j<rows; j++){
         layer = layer.substring(1)
         layer += (brick);
-        console.log(layer);
-      
-    
-    }
 
+         document.write( layer + " <br>");
+    }
 }
+
+
+// function printPyramid(height) {
+//     let space = ".";
+//     let brick = "#";
+//     let rows = height;
+
+//     let layer = space;
+//     for (let i = 1; i<rows ; i++){
+//       layer += space;
+//     }
+//     layer += brick;
+//       for(let j=0; j<rows; j++){
+//         layer = layer.substring(1)
+//         layer += (brick);
+//         console.log(layer);
+//     }
+
+// }
+
+const para = document.createElement("p");
+
+const node = document.createTextNode(drawPyramid(height) );
+
+// para.appndChild(node);
+const element = document.getElementById("pyramid");
+
+element.appendChild(para);
